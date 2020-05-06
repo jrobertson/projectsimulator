9f68741eed0a1ac9fe61ff5c4399237e5d3ee604
James Robertson <james@jamesrobertson.eu>
2020-05-06 01:08:17 +0100

    feature: Implemented the Controller class.

--- lib/projectsimulator.rb
+++ lib/projectsimulator.rb
@@ -2,8 +2,10 @@
 
 # file: projectsimulator.rb
 
+require 'rowx'
 require 'easydom'
 require 'app-routes'
+require 'chronic_between'
 
 
 module ProjectSimulator
@@ -47,7 +49,7 @@ module ProjectSimulator
     def request(s)
 
       params = {request: s}
-      requests(@params)
+      requests(params)
       h = find_request(s)
 
       method(h.first[-1]).call(h)
@@ -99,6 +101,19 @@ module ProjectSimulator
         location = dev_location(device)        
         {type: :get_device, action: 'switch', location: location, device: device}
       end            
+      
+      # e.g. fetch the livingroom temperature reading
+      #
+      get /fetch the ([^ ]+) +([^ ]+) +(?:reading)$/ do |location, device|
+        {type: :get_device, action: 'reading', location: location, device: device}
+      end
+
+      # e.g. fetch the temperature reading
+      #
+      get /fetch the ([^ ]+) +(?:reading)$/ do |device|
+        location = dev_location(device)        
+        {type: :get_device, action: 'reading', location: location, device: device}
+      end          
 
     end
     
@@ -112,4 +127,258 @@ module ProjectSimulator
     alias find_request run_route    
 
   end
+  
+  
+  class Event
+
+    attr_accessor :title
+    attr_reader :triggers, :actions, :constraints, :messages
+
+    def initialize(e, time: nil, debug: false)    
+
+      @time, @debug = time, debug
+      @title = e.text('event')
+      @actions = []
+      @triggers = []
+      @constraints = []
+
+      e.xpath('trigger/text()').each do |x| 
+        @triggers << Trigger.new(x, time: time, debug: debug).to_type
+      end
+
+      e.xpath('action/text()').each do |x|
+        @actions << Action.new(x, debug: debug).to_type
+      end
+
+      e.xpath('constraint/text()').each do |x|
+        puts 'before Constraints.new'
+        @constraints << Constraint.new(x, time: time, debug: debug).to_type
+      end
+
+    end
+    
+    def match(trigger: nil, location: '')
+      
+      @messages = []
+      
+      h = {motion: MotionTrigger}
+      if @triggers.any? {|x| x.is_a? h[trigger.to_sym] and x.match } and \
+          @constraints.all?(&:match) then
+        
+        @messages = @actions.map(&:call)
+        
+      end
+      
+    end
+
+  end
+
+  class Action
+    include AppRoutes
+
+    attr_reader :to_type
+
+    def initialize(s, event: '', debug: false)
+
+      super()
+      @debug = debug
+      params = {s: s, event: event}
+      actions(params)
+      @to_type = find_action(s) || {}
+
+    end
+
+    protected
+
+    def actions(params) 
+
+      puts 'inside actions'
+      # e.g. Say 'Good morning'
+      #
+      get /say ['"]([^'"]+)/i do |s|
+        puts 's: ' + s.inspect if @debug
+        SayAction.new(s)
+      end
+      
+      # e.g. webhook entered_kitchen
+      #
+      get /webhook (.*)/i do |name|
+        WebhookAction.new(name)
+      end      
+      
+      get /.*/ do
+        puts 'action unknown' if @debug
+        {}
+      end
+
+    end
+
+    private
+
+    alias find_action run_route
+
+  end
+
+  class Trigger
+    include AppRoutes
+
+    attr_reader :to_type
+
+    def initialize(s, time: nil, debug: false)
+
+      super()
+      @time, @debug = time, debug
+      params = {s: s}
+      puts 'inside Trigger'
+      puts 'params: ' + params.inspect
+      triggers(params)
+      @to_type = find_trigger(s)
+
+    end
+
+    protected
+
+    def triggers(params) 
+      
+      puts 'inside triggers'
+
+      # e.g. Motion detected in the kitchen
+      #
+      get /motion detected in the (.*)/i do |location|
+        puts 'motion detection trigger'
+        MotionTrigger.new(location)
+      end
+
+    end
+
+    private
+
+    alias find_trigger run_route
+
+  end
+  
+  class Constraint
+    include AppRoutes
+    
+    attr_reader :to_type
+    
+    def initialize(s, time: nil, debug: false)
+      
+      super()
+      @time, @debug = time, debug
+      
+      params = {s: s }
+      constraints(params)
+      @to_type = find_constraint(s)
+      
+    end
+    
+    protected
+
+    def constraints(params) 
+
+      puts 'inside constraints' if @debug
+      # e.g. Between 8am and 10am
+      #
+      get /^between (.*)/i do |s|
+        TimeConstraint.new(s, time: @time)
+      end
+      
+      get /^on a (.*)/i do |s|
+        TimeConstraint.new(s, time: @time)
+      end
+      
+      get /^(after .*)/i do |s|
+        TimeConstraint.new(s, time: @time)
+      end      
+
+    end
+
+    private
+
+    alias find_constraint run_route    
+  end
+
+  class MotionTrigger
+
+    attr_reader :location
+    
+    def initialize(location)
+      @location = location
+    end
+
+    def match()
+      @location.downcase == location.downcase
+    end
+
+  end
+
+  class SayAction
+
+    def initialize(s)
+      @s = s
+    end
+    
+    def call()
+      "say: %s" % @s
+    end
+
+  end
+  
+  class WebhookAction
+    
+    attr_accessor :url
+
+    def initialize(name)
+      @name = name
+      @url = '127.0.0.1'
+    end
+    
+    def call()
+      "webhook: %s" % @url
+    end
+
+  end
+
+  class TimeConstraint
+    
+    attr_reader :match
+    
+    def initialize(times, time: nil)
+      @match = ChronicBetween.new(times).within?(time)
+    end
+        
+  end  
+
+  class Controller
+    
+    attr_reader :events
+    attr_accessor :time
+
+    def initialize(s, time: Time.now, debug: false)
+      
+      @time, @debug = time, debug
+
+      doc = Rexle.new(RowX.new(s).to_xml)
+
+      @events = doc.root.xpath('item')\
+          .map {|e| Event.new(e, time: @time, debug: debug) }
+
+    end
+    
+    def trigger(name, location: '')
+      
+      events = @events.select do |event|
+        
+        puts 'event: '  + event.inspect if @debug
+
+        event.match(trigger: 'motion', location: location='kitchen')
+        
+      end
+      
+      events.flat_map(&:messages)
+      
+    end
+
+  end  
 end
--- projectsimulator.gemspec
+++ projectsimulator.gemspec
@@ -1,12 +1,14 @@
 Gem::Specification.new do |s|
   s.name = 'projectsimulator'
-  s.version = '0.1.1'
+  s.version = '0.2.0'
   s.summary = 'Project Simulator (in development) aims to make it easier to observe ' + 
       'triggers and actions from an XML based model.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/projectsimulator.rb']
+  s.add_runtime_dependency('rowx', '~> 0.7', '>=0.7.0')
   s.add_runtime_dependency('easydom', '~> 0.2', '>=0.2.1')
   s.add_runtime_dependency('app-routes', '~> 0.1', '>=0.1.19')
+  s.add_runtime_dependency('chronic_between', '~> 0.3', '>=0.3.1')
   s.signing_key = '../privatekeys/projectsimulator.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
